0d764db311864612da6959fe4137de18c7f6db4a
Jess <jess@sulliwood.org>
2026-02-09 08:00:32 -0500

    ci(sync): wire up external repo sync with file stripping

--- version.go
+++ version.go
@@ -2,9 +2,9 @@ package main
 
 // Build-time variables, set via ldflags:
 //
-//	go build -ldflags "-X main.version=2.0.0 -X main.commit=$(git rev-parse --short HEAD) -X main.date=$(date -u +%Y-%m-%dT%H:%M:%SZ)"
+//	go build -ldflags "-X main.version=2.0.1 -X main.commit=$(git rev-parse --short HEAD) -X main.date=$(date -u +%Y-%m-%dT%H:%M:%SZ)"
 var (
-	version = "2.0.0"
+	version = "2.0.1"
 	commit  = "dev"
 	date    = "unknown"
 )
